a664ca3112159153b352af91497b430fbd26dc90
Timur Bazhirov <timur@exabyte.io>
2022-06-09 18:41:18 -0700

    feature: add hash util

---
+++ src/utils/hash.js
@@ -0,0 +1,32 @@
+import _ from "underscore";
+import CryptoJS from "crypto-js";
+import { sortKeysDeepForObject } from "./object";
+
+/**
+ * @summary Calculates hash of a given text.
+ * @param message {String} message to hash
+ * @param hashFunction {String} hash function name.
+ * @return {String}
+ */
+export function calculateHashFromString(message, hashFunction = "MD5") {
+    switch (hashFunction) {
+        case "MD5":
+        default:
+            return CryptoJS.MD5(message).toString();
+    }
+}
+
+/**
+ * @summary Calculates hash of a given object.
+ * @param obj {Object} object to hash. It must be serializable.
+ * @param hashFunction {String} hash function name.
+ * @return {String}
+ */
+export function calculateHashFromObject(obj, hashFunction = "MD5") {
+    const message = JSON.stringify(sortKeysDeepForObject(obj));
+    return calculateHashFromString(message, hashFunction);
+}
+
+export function removeTimestampableKeysFromConfig(config) {
+    return _.omit(config, ["createdAt", "updatedAt", "removedAt"]);
+}
