69871779b6451823359d43526075fd09f2374d60
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2022-10-20 02:10:57 +0300

    fix: clean schemaId before mergeAllOf

--- tests/JSONSchemasInterface.tests.js
+++ tests/JSONSchemasInterface.tests.js
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
 
 import { JSONSchemasInterface } from "../src/JSONSchemasInterface";
 
@@ -20,15 +20,55 @@ describe("JSONSchemasInterface", () => {
 
     it("can find registered schemas", () => {
         JSONSchemasInterface.registerSchema({
-            schemaId: "test-schema-id",
+            schemaId: "system/in-set",
+            $schema: "http://json-schema.org/draft-04/schema#",
+            title: "System in-set schema",
             properties: {
-                testProp: {
-                    type: "string",
+                inSet: {
+                    type: "array",
+                    items: {
+                        allOf: [
+                            {
+                                schemaId: "system/entity-reference",
+                                $schema: "http://json-schema.org/draft-04/schema#",
+                                title: "entity reference schema",
+                                properties: {
+                                    _id: {
+                                        description: "entity identity",
+                                        type: "string",
+                                    },
+                                    cls: {
+                                        description: "entity class",
+                                        type: "string",
+                                    },
+                                    slug: {
+                                        description: "entity slug",
+                                        type: "string",
+                                    },
+                                },
+                            },
+                            {
+                                type: "object",
+                                properties: {
+                                    type: {
+                                        type: "string",
+                                    },
+                                    index: {
+                                        type: "number",
+                                    },
+                                },
+                            },
+                        ],
+                    },
                 },
             },
         });
 
-        const schema = JSONSchemasInterface.schemaById("test-schema-id");
+        const schema = JSONSchemasInterface.schemaById("system/in-set");
+
         expect(schema).to.be.an("object");
+        assert(schema.schemaId, "system/in-set");
+        expect(schema.properties.inSet.items.schemaId).to.be.an("undefined");
+        expect(schema.properties.inSet.items.properties).to.be.an("object");
     });
 });
