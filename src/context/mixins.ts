f077c6c2529412c99b87e8119d80b31de2a16eeb
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2023-07-31 13:12:02 +0300

    chore: context mixin old code

--- src/context/mixins.ts
+++ src/context/mixins.ts
@@ -1,58 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// @ts-nocheck
+/* eslint-disable max-classes-per-file */
 import CryptoJS from "crypto-js";
 
-import { DefaultableMixin } from "../entity/mixins/props";
 import { compareEntitiesInOrderedSetForSorting } from "../entity/set/ordered/utils";
 
-type Constructor<T = any> = new (...args: any[]) => T;
-
-type Defaultable = ReturnType<typeof DefaultableMixin>;
-
-export function ApplicationContextMixin<T extends Constructor>(superclass: T) {
-    return class ApplicationContextMixin extends superclass {
-        _application: any;
-
-        constructor(...args: any) {
-            super(...args);
-            // @ts-ignore
+export const ApplicationContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             if (!this.constructor.Application) {
                 throw Error("ApplicationContextMixin: Application is undefined");
             }
-            const config = args[0];
             this._application =
                 (config.context && config.context.application) ||
-                // @ts-ignore
-                (this.constructor.Application as Defaultable).createDefault();
+                this.constructor.Application.createDefault();
         }
 
         get application() {
             return this._application;
         }
     };
-}
 
-export function MaterialContextMixin<T extends Constructor>(superclass: T) {
-    return class MaterialContextMixin extends superclass {
-        _material: any;
-
-        extraData?: {
-            materialHash: string;
-        };
-
-        isEdited?: boolean;
-
-        constructor(...args: any) {
-            super(...args);
-            // @ts-ignore
+export const MaterialContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             if (!this.constructor.Material) {
                 throw Error("MaterialContextMixin: Material is undefined");
             }
-            const config = args[0];
             this._material = config.context && config.context.material;
-            // @ts-ignore
-            if (!this._material)
-                // @ts-ignore
-                this._material = (this.constructor.Material as Defaultable).createDefault();
+            if (!this._material) this._material = this.constructor.Material.createDefault();
             this.updateMaterialHash();
         }
 
@@ -80,14 +57,11 @@ export function MaterialContextMixin<T extends Constructor>(superclass: T) {
             return this._material;
         }
     };
-}
 
-export function MaterialsSetContextMixin<T extends Constructor>(superclass: T) {
-    return class MaterialsSetContextMixin extends superclass {
-        _materialsSet: any;
-
-        constructor(...params: any) {
-            super(...params);
+export const MaterialsSetContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             this._materialsSet = this.config.context && this.config.context.materialsSet;
         }
 
@@ -102,49 +76,30 @@ export function MaterialsSetContextMixin<T extends Constructor>(superclass: T) {
             });
         }
     };
-}
 
-export function MaterialsContextMixin<T extends Constructor>(superclass: T) {
-    return class MaterialsContextMixin extends superclass {
-        _materials: any;
-
-        constructor(...params: any) {
-            super(...params);
+export const MaterialsContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             const materials = this.config.context && this.config.context.materials;
-            // @ts-ignore
             if (!this.constructor.Material) {
                 throw Error("MaterialsContextMixin: Material is undefined");
             }
             this._materials =
                 materials && materials.length
                     ? materials
-                    : // @ts-ignore
-                      [this.constructor.Material.createDefault()];
+                    : [this.constructor.Material.createDefault()];
         }
 
         get materials() {
             return this._materials;
         }
     };
-}
 
-export function MethodDataContextMixin<T extends Constructor>(superclass: T) {
-    return class extends superclass {
-        _methodData: any;
-
-        isEdited: boolean;
-
-        methodDataHash?: string;
-
-        extraData?: {
-            methodDataHash?: string;
-        };
-
-        constructor(...params: any) {
-            super(...params);
-
-            const config = params[0];
-
+export const MethodDataContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             this._methodData = (config.context && config.context.methodData) || {};
             this.isEdited = false; // we always get the `defaultData` (recalculated from scratch, not persistent)
         }
@@ -164,7 +119,6 @@ export function MethodDataContextMixin<T extends Constructor>(superclass: T) {
             } else if (this.isMethodDataUpdated) {
                 this.isEdited = false;
             } else {
-                // @ts-ignore
                 // eslint-disable-next-line no-undef
                 this.isEdited = config.isEdited;
             }
@@ -178,19 +132,11 @@ export function MethodDataContextMixin<T extends Constructor>(superclass: T) {
             return Boolean(this.extraData && this.extraData.methodDataHash !== this.methodDataHash);
         }
     };
-}
 
-export function WorkflowContextMixin<T extends Constructor>(superclass: T) {
-    return class extends superclass {
-        _workflow: any;
-
-        isEdited: boolean;
-
-        constructor(...params: any) {
-            super(...params);
-
-            const config = params[0];
-
+export const WorkflowContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             this._workflow = (config.context && config.context.workflow) || {};
             this.isEdited = false; // we always get the `defaultData` (recalculated from scratch, not persistent)
         }
@@ -199,19 +145,11 @@ export function WorkflowContextMixin<T extends Constructor>(superclass: T) {
             return this._workflow;
         }
     };
-}
 
-export function JobContextMixin<T extends Constructor>(superclass: T) {
-    return class extends superclass {
-        _job: any;
-
-        isEdited: boolean;
-
-        constructor(...params: any) {
-            super(...params);
-
-            const config = params[0];
-
+export const JobContextMixin = (superclass) =>
+    class extends superclass {
+        constructor(config) {
+            super(config);
             this._job = (config.context && config.context.job) || {};
             this.isEdited = false; // we always get the `defaultData` (recalculated from scratch, not persistent)
         }
@@ -220,4 +158,3 @@ export function JobContextMixin<T extends Constructor>(superclass: T) {
             return this._job;
         }
     };
-}
