1792c6c7fd0676f497166542eb4cfd114824607b
Alexander Zech <alexzech@gmx.de>
2023-08-01 18:08:03 -0700

    test: string concatenation in Yaml

--- tests/enums.js
+++ tests/enums.js
@@ -7,3 +7,4 @@ export const YAML_ESSE_FILE = path.resolve(FIXTURES_DIR, "yaml_esse_tag.yml");
 export const YAML_INCLUDE_FILE = path.resolve(FIXTURES_DIR, "yaml_include_tag.yml");
 export const YAML_FLATTEN_FILE = path.resolve(FIXTURES_DIR, "yaml_flatten_tag.yml");
 export const YAML_READFILE_FILE = path.resolve(FIXTURES_DIR, "yaml_readFile_tag.yml");
+export const YAML_CONCAT_STRING_FILE = path.resolve(FIXTURES_DIR, "yaml_concatString_tag.yml");
