3977b92a56295ba5ce1fc3b51bd1fcb59518848a
Alexander Zech <alexzech@gmx.de>
2023-08-30 18:15:42 -0700

    chore: increase timeout for before hook

--- tests/utils/yaml.combine.tests.ts
+++ tests/utils/yaml.combine.tests.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-expressions */
 // @ts-nocheck
 import { expect } from "chai";
-import fs from "fs";
+import fs from "fs/promises";
 import yaml from "js-yaml";
 import lodash from "lodash";
 
@@ -13,8 +13,10 @@ const combineSchema = yaml.DEFAULT_SCHEMA.extend([combineType, esseType]);
 describe("YAML tag: !combine", () => {
     let yamlFixture;
 
-    before(() => {
-        yamlFixture = fs.readFileSync(YAML_COMBINE_FILE, "utf8");
+    // eslint-disable-next-line func-names
+    before(async function () {
+        this.timeout(5000);
+        yamlFixture = await fs.readFile(YAML_COMBINE_FILE, "utf8");
     });
 
     it("should correctly parse a custom !combine tag with forEach and config keys", () => {
