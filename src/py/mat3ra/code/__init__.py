cb21f7aa442a4e831cbb7c3e858a61ed71b6afa6
Timur Bazhirov <timur@exabyte.io>
2024-04-03 15:33:22 -0700

    update: add in_memory_entity tests

--- src/py/mat3ra/code/__init__.py
+++ src/py/mat3ra/code/__init__.py
@@ -1,35 +1,8 @@
 from typing import Any, Dict, List, Optional, Union
-import copy
-import json
+
 import jsonschema
 
-
-class JSONSchema:
-    pass
-
-
-class EntityReferenceSchema:
-    pass
-
-
-def getValue(obj: Dict[str, Any], key: str, default: Any) -> Any:
-    return obj.get(key, default)
-
-
-def omit(obj: Dict[str, Any], keys: List[str]) -> Dict[str, Any]:
-    return {k: v for k, v in obj.items() if k not in keys}
-
-
-def set(obj: Dict[str, Any], key: str, value: Any) -> None:
-    obj[key] = value
-
-
-def clone(obj: Any) -> Any:
-    return copy.deepcopy(obj)
-
-
-def deepClone(obj: Any) -> Any:
-    return copy.deepcopy(obj)
+from mat3ra.utils import object as object_utils
 
 
 class ValidationErrorCode:
@@ -37,7 +10,7 @@ class ValidationErrorCode:
 
 
 class ErrorDetails:
-    def __init__(self, error: Optional[Dict[str, Any]], json: Dict[str, Any], schema: JSONSchema):
+    def __init__(self, error: Optional[Dict[str, Any]], json: Dict[str, Any], schema: Dict):
         self.error = error
         self.json = json
         self.schema = schema
@@ -51,72 +24,64 @@ class EntityError(Exception):
 
 
 class InMemoryEntity:
+    _isDeepCloneRequired = False
+    allowJsonSchemaTypesCoercing = False
+    jsonSchema: Optional[Dict] = None
+
     @staticmethod
     def create(config: Dict[str, Any]) -> 'InMemoryEntity':
         return InMemoryEntity(config)
 
-    _isDeepCloneRequired = False
-    allowJsonSchemaTypesCoercing = False
-    jsonSchema: Optional[JSONSchema] = None
-
     def __init__(self, config: Dict[str, Any] = {}) -> None:
-        self._json = (self.__class__._isDeepCloneRequired and deepClone(config)) or clone(config)
+        self._json = object_utils.clone_deep(
+            config) if self.__class__._isDeepCloneRequired else object_utils.clone_shallow(config)
 
-    def prop(self, name: str, defaultValue: Any = None) -> Any:
-        return getValue(self._json, name, defaultValue) or defaultValue
+    def prop(self, name: str, default_value: Any = None) -> Any:
+        return self._json.get(name, default_value)
 
-    def setProp(self, name: str, value: Any) -> None:
-        set(self._json, name, value)
+    def set_prop(self, name: str, value: Any) -> None:
+        object_utils.set(self._json, name, value)
 
-    def unsetProp(self, name: str) -> None:
+    def unset_prop(self, name: str) -> None:
         del self._json[name]
 
-    def setProps(self, json: Dict[str, Any] = {}) -> 'InMemoryEntity':
+    def set_props(self, json: Dict[str, Any] = {}) -> 'InMemoryEntity':
         for key, value in json.items():
-            self.setProp(key, value)
+            self.set_prop(key, value)
         return self
 
-    def toJSON(self, exclude: List[str] = []) -> Dict[str, Any]:
+    def to_json(self, exclude: List[str] = []) -> Dict[str, Any]:
         if self.__class__._isDeepCloneRequired:
-            return self.toJSONSafe(exclude)
+            return self.to_json_safe(exclude)
         else:
-            return self.toJSONQuick(exclude)
+            return self.to_json_quick(exclude)
 
-    def toJSONSafe(self, exclude: List[str] = []) -> Dict[str, Any]:
-        return self.clean(deepClone(omit(self._json, exclude)))
+    def to_json_safe(self, exclude: List[str] = []) -> Dict[str, Any]:
+        return self.clean(object_utils.clone_deep(object_utils.omit(self._json, exclude)))
 
-    def toJSONQuick(self, exclude: List[str] = []) -> Dict[str, Any]:
-        return self.clean(clone(omit(self._json, exclude)))
+    def to_json_quick(self, exclude: List[str] = []) -> Dict[str, Any]:
+        return self.clean(object_utils.clone_shallow(object_utils.omit(self._json, exclude)))
 
-    def clone(self, extraContext: Dict[str, Any] = {}) -> 'InMemoryEntity':
-        return self.__class__({**self.toJSON(), **extraContext})
+    def clone(self, extra_context: Dict[str, Any] = {}) -> 'InMemoryEntity':
+        return self.__class__({**self.to_json(), **extra_context})
 
     @staticmethod
-    def validateData(data: Dict[str, Any], clean: bool = False) -> Dict[str, Any]:
-        if not InMemoryEntity.jsonSchema:
-            return data
+    def validate_data(data: Dict[str, Any], clean: bool = False):
         if clean:
             print("Error: clean is not supported for InMemoryEntity.validateData")
-        else:
+        if InMemoryEntity.jsonSchema:
             jsonschema.validate(data, InMemoryEntity.jsonSchema)
-        return data
 
     def validate(self) -> None:
         if self._json:
-            self.__class__.validateData(self._json)
+            self.__class__.validate_data(self._json)
 
     def clean(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        try:
-            return self.__class__.validateData(config, True)
-        except EntityError as err:
-            print(json.dumps({
-                'error': json.dumps(err.details.error),
-                'json': json.dumps(err.details.json),
-                'schema': json.dumps(err.details.schema),
-            }))
-            raise
+        # Not implemented, consider the below for the implementation
+        # https://stackoverflow.com/questions/44694835/remove-properties-from-json-object-not-present-in-schema
+        return config
 
-    def isValid(self) -> bool:
+    def is_valid(self) -> bool:
         try:
             self.validate()
             return True
@@ -129,7 +94,7 @@ class InMemoryEntity:
 
     @id.setter
     def id(self, id: str) -> None:
-        self.setProp("_id", id)
+        self.set_prop("_id", id)
 
     @classmethod
     def get_cls(cls) -> str:
@@ -167,4 +132,3 @@ class InMemoryEntity:
         if len(filtered) != 1:
             print(f"found {len(filtered)} entity {entity} with name {name} expected 1")
         return filtered[0]
-
