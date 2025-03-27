090f03ea5045aa6e11685469a837ed184b03ad4d
VsevolodX <vsevolodbiryukov@icloud.com>
2025-03-26 20:49:39 -0700

    update: start using pydantic

--- src/py/mat3ra/code/entity.py
+++ src/py/mat3ra/code/entity.py
@@ -1,11 +1,11 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, TypeVar
 
 import jsonschema
-from mat3ra.utils import object as object_utils
+from pydantic import BaseModel, Field
 
-from . import BaseUnderscoreJsonPropsHandler
 from .mixins import DefaultableMixin, HasDescriptionMixin, HasMetadataMixin, NamedMixin
 
+T = TypeVar("T", bound="InMemoryEntity")
 
 class ValidationErrorCode:
     IN_MEMORY_ENTITY_DATA_INVALID = "IN_MEMORY_ENTITY_DATA_INVALID"
@@ -25,59 +25,25 @@ class EntityError(Exception):
         self.details = details
 
 
-class InMemoryEntity(BaseUnderscoreJsonPropsHandler):
-    jsonSchema: Optional[Dict] = None
 
+class InMemoryEntity(BaseModel):
+    jsonSchema: Optional[Dict[str, Any]] = Field(default=None, exclude=True)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    # --- Identity and Meta ---
     @classmethod
     def get_cls(cls) -> str:
         return cls.__name__
+
+    def get_cls_name(self) -> str:
+        return self.__class__.__name__
 
     @property
     def cls(self) -> str:
         return self.__class__.__name__
 
-    def get_cls_name(self) -> str:
-        return self.__class__.__name__
-
-    @classmethod
-    def create(cls, config: Dict[str, Any]) -> Any:
-        return cls(config)
-
-    def to_json(self, exclude: List[str] = []) -> Dict[str, Any]:
-        return self.clean(object_utils.clone_deep(object_utils.omit(self._json, exclude)))
-
-    def clone(self, extra_context: Dict[str, Any] = {}) -> Any:
-        config = self.to_json()
-        config.update(extra_context)
-        # To avoid:
-        #   Argument 1 to "__init__" of "BaseUnderscoreJsonPropsHandler" has incompatible type "Dict[str, Any]";
-        #   expected "BaseUnderscoreJsonPropsHandler"
-        return self.__class__(config)
-
-    @staticmethod
-    def validate_data(data: Dict[str, Any], clean: bool = False):
-        if clean:
-            print("Error: clean is not supported for InMemoryEntity.validateData")
-        if InMemoryEntity.jsonSchema:
-            jsonschema.validate(data, InMemoryEntity.jsonSchema)
-
-    def validate(self) -> None:
-        if self._json:
-            self.__class__.validate_data(self._json)
-
-    def clean(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        # Not implemented, consider the below for the implementation
-        # https://stackoverflow.com/questions/44694835/remove-properties-from-json-object-not-present-in-schema
-        return config
-
-    def is_valid(self) -> bool:
-        try:
-            self.validate()
-            return True
-        except EntityError:
-            return False
-
-    # Properties
     @property
     def id(self) -> str:
         return self.prop("_id", "")
@@ -91,10 +57,56 @@ class InMemoryEntity(BaseUnderscoreJsonPropsHandler):
         return self.prop("slug", "")
 
     def get_as_entity_reference(self, by_id_only: bool = False) -> Dict[str, str]:
-        if by_id_only:
-            return {"_id": self.id}
-        else:
-            return {"_id": self.id, "slug": self.slug, "cls": self.get_cls_name()}
+        base = {"_id": self.id}
+        if not by_id_only:
+            base.update({"slug": self.slug, "cls": self.get_cls_name()})
+        return base
+
+    # --- Serialization ---
+    def to_dict(self, exclude: Optional[List[str]] = None) -> Dict[str, Any]:
+        return self.model_dump(exclude=set(exclude) if exclude else None)
+
+    def to_json(self, exclude: Optional[List[str]] = None) -> str:
+        return self.model_dump_json(exclude=set(exclude) if exclude else None)
+
+    # --- Instantiation ---
+    @classmethod
+    def create(cls: type[T], config: Dict[str, Any]) -> T:
+        return cls(**config)
+
+    def clone(self: T, extra_context: Optional[Dict[str, Any]] = None) -> T:
+        """Return a copy of this entity with optional updated fields."""
+        extra_context = extra_context or {}
+        return self.model_copy(update=extra_context)
+
+    # --- Validation ---
+    def validate(self) -> None:
+        if self._json and self.jsonSchema:
+            try:
+                jsonschema.validate(self._json, self.jsonSchema)
+            except jsonschema.exceptions.ValidationError as err:
+                raise EntityError(
+                    ValidationErrorCode.IN_MEMORY_ENTITY_DATA_INVALID,
+                    ErrorDetails(error=err, json=self._json, schema=self.jsonSchema),
+                )
+
+    @staticmethod
+    def validate_data(data: Dict[str, Any], clean: bool = False):
+        if clean:
+            raise NotImplementedError("clean=True not supported yet.")
+        if InMemoryEntity.jsonSchema:
+            jsonschema.validate(data, InMemoryEntity.jsonSchema)
+
+    def is_valid(self) -> bool:
+        try:
+            self.validate()
+            return True
+        except EntityError:
+            return False
+
+    def clean(self, config: Dict[str, Any]) -> Dict[str, Any]:
+        # TODO: implement if needed, or use model_rebuild if schema evolves
+        return config
 
 
 class HasDescriptionHasMetadataNamedDefaultableInMemoryEntity(
