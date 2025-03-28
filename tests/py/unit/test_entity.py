c253b0bad84c5ad1a03c49855b118586a609b110
VsevolodX <vsevolodbiryukov@icloud.com>
2025-03-27 20:15:39 -0700

    chore: adjsut test import

--- tests/py/unit/test_entity.py
+++ tests/py/unit/test_entity.py
@@ -1,16 +1,19 @@
-from mat3ra.code.entity import InMemoryEntity
+from mat3ra.code.entity import InMemoryEntityPydantic
 
 REFERENCE_OBJECT_1 = {"key1": "value1", "key2": "value2"}
 
-class ChildClass(InMemoryEntity):
+
+class ChildClass(InMemoryEntityPydantic):
     key1: str
     key2: str
 
+
 child_object = ChildClass(**REFERENCE_OBJECT_1)
 
+
 def test_create():
-    in_memory_entity = InMemoryEntity.create({})
-    assert isinstance(in_memory_entity, InMemoryEntity)
+    in_memory_entity = InMemoryEntityPydantic.create({})
+    assert isinstance(in_memory_entity, InMemoryEntityPydantic)
 
 
 def test_subclass_fields():
