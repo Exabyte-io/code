811c20cfdd5c96156f1ba7f88a2bd6f5dffd5b49
VsevolodX <vsevolodbiryukov@icloud.com>
2025-03-28 17:25:42 -0700

    update: more tests

--- tests/py/unit/test_entity.py
+++ tests/py/unit/test_entity.py
@@ -39,6 +39,49 @@ def test_validate():
         assert True  # Expecting an exception for invalid input
 
 
+def test_is_valid():
+    assert ExampleClass.is_valid(REFERENCE_OBJECT_VALID) is True
+    assert ExampleClass.is_valid(REFERENCE_OBJECT_INVALID) is False
+
+
+def test_from_json():
+    # Test from_json method with valid JSON
+    in_memory_entity = ExampleClass.from_json(REFERENCE_OBJECT_VALID_JSON)
+    assert isinstance(in_memory_entity, ExampleClass)
+    assert in_memory_entity.key1 == "value1"
+    assert in_memory_entity.key2 == 1
+
+    # Test from_json with invalid JSON
+    try:
+        _ = ExampleClass.from_json(json.dumps(REFERENCE_OBJECT_INVALID))
+        assert False, "Invalid JSON did not raise an exception"
+    except Exception:
+        assert True  # Expecting an exception for invalid JSON
+
+
+def test_clean():
+    # Test clean method with valid input
+    cleaned_data = ExampleClass.clean(REFERENCE_OBJECT_VALID)
+    assert isinstance(cleaned_data, dict)
+    assert cleaned_data == REFERENCE_OBJECT_VALID
+
+    # Test clean method with invalid input
+    try:
+        _ = ExampleClass.clean(REFERENCE_OBJECT_INVALID)
+        assert False, "Invalid input did not raise an exception"
+    except Exception:
+        assert True  # Expecting an exception for invalid input
+
+
+def test_get_cls_name():
+    # Test get_cls_name method
+    entity = ExampleClass.create(REFERENCE_OBJECT_VALID)
+    cls_name = entity.get_cls_name()
+    assert cls_name == "ExampleClass", f"Expected 'ExampleClass', got '{cls_name}'"
+    # Ensure it works for the class itself
+    assert ExampleClass.__name__ == "ExampleClass"
+
+
 def test_to_dict():
     entity = ExampleClass.create(REFERENCE_OBJECT_VALID)
     # Test to_dict method
@@ -52,7 +95,22 @@ def test_to_dict():
 
 def test_to_json():
     entity = ExampleClass.create(REFERENCE_OBJECT_VALID)
-
     result = entity.to_json()
     assert isinstance(result, str)
     assert json.loads(result) == json.loads(REFERENCE_OBJECT_VALID_JSON)
+
+
+def test_clone():
+    entity = ExampleClass.create(REFERENCE_OBJECT_VALID)
+    # Test clone method
+    cloned_entity = entity.clone()
+    assert isinstance(cloned_entity, ExampleClass)
+    assert cloned_entity.key1 == entity.key1
+    assert cloned_entity.key2 == entity.key2
+
+    # Test clone with extra context
+    extra_context = {"key2": 2}
+    cloned_entity_with_extra = entity.clone(extra_context=extra_context)
+    assert isinstance(cloned_entity_with_extra, ExampleClass)
+    assert cloned_entity_with_extra.key1 == entity.key1
+    assert cloned_entity_with_extra.key2 == 2  # Should override to 2
