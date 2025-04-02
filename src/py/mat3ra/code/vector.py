a9114023c76b86abd86979c71e2cfaf0f91837f0
VsevolodX <vsevolodbiryukov@icloud.com>
2025-04-02 16:37:59 -0700

    chore: add xyz to vector

--- src/py/mat3ra/code/vector.py
+++ src/py/mat3ra/code/vector.py
@@ -13,6 +13,18 @@ class Vector3D(Vector3DSchema):
     def value(self):
         return self.root
 
+    @property
+    def x(self):
+        return self.root[0]
+
+    @property
+    def y(self):
+        return self.root[1]
+
+    @property
+    def z(self):
+        return self.root[2]
+
 
 class RoundedVector3D(RoundNumericValuesMixin, Vector3D):
     def __init__(self, root: List[float]):
@@ -26,3 +38,15 @@ class RoundedVector3D(RoundNumericValuesMixin, Vector3D):
     @property
     def value_rounded(self):
         return self.to_dict()
+
+    @property
+    def x_rounded(self):
+        return self.value_rounded[0]
+
+    @property
+    def y_rounded(self):
+        return self.value_rounded[1]
+
+    @property
+    def z_rounded(self):
+        return self.value_rounded[2]
--- tests/py/unit/test_vector.py
+++ tests/py/unit/test_vector.py
@@ -8,6 +8,10 @@ VECTOR_FLOAT_ROUNDED_3 = [1.235, 2.346, 3.457]
 def test_vector_init():
     vector = Vector3D(VECTOR_FLOAT)
     assert vector.model_dump() == VECTOR_FLOAT
+    assert vector.value == VECTOR_FLOAT
+    assert vector.x == 1.234567890
+    assert vector.y == 2.345678901
+    assert vector.z == 3.456789012
 
 
 def test_vector_init_wrong_type():
@@ -28,6 +32,7 @@ def test_vector_init_wrong_size():
 def test_rounded_vector_init():
     vector = RoundedVector3D(VECTOR_FLOAT)
     assert vector.model_dump() == VECTOR_FLOAT
+    assert vector.value == VECTOR_FLOAT
 
 
 def test_rounded_vector_serialization():
@@ -36,6 +41,12 @@ def test_rounded_vector_serialization():
     vector = class_reference(VECTOR_FLOAT)
     assert vector.model_dump() == VECTOR_FLOAT_ROUNDED_4
     assert vector.value_rounded == VECTOR_FLOAT_ROUNDED_4
+    assert vector.x == VECTOR_FLOAT[0]
+    assert vector.y == VECTOR_FLOAT[1]
+    assert vector.z == VECTOR_FLOAT[2]
+    assert vector.x_rounded == VECTOR_FLOAT_ROUNDED_4[0]
+    assert vector.y_rounded == VECTOR_FLOAT_ROUNDED_4[1]
+    assert vector.z_rounded == VECTOR_FLOAT_ROUNDED_4[2]
     assert vector.value == VECTOR_FLOAT
 
     class_reference = RoundedVector3D
@@ -44,3 +55,9 @@ def test_rounded_vector_serialization():
     assert vector.model_dump() == VECTOR_FLOAT_ROUNDED_3
     assert vector.value_rounded == VECTOR_FLOAT_ROUNDED_3
     assert vector.value == VECTOR_FLOAT
+    assert vector.x == VECTOR_FLOAT[0]
+    assert vector.y == VECTOR_FLOAT[1]
+    assert vector.z == VECTOR_FLOAT[2]
+    assert vector.x_rounded == VECTOR_FLOAT_ROUNDED_3[0]
+    assert vector.y_rounded == VECTOR_FLOAT_ROUNDED_3[1]
+    assert vector.z_rounded == VECTOR_FLOAT_ROUNDED_3[2]
