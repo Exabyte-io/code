f1c1ac1eadb2e09cd74506326d2acbb5972c6c5e
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2025-07-24 18:48:26 +0300

    update: improve InMemoryEntityInSetMixin

--- src/js/entity/set/InMemoryEntityInSetMixin.ts
+++ src/js/entity/set/InMemoryEntityInSetMixin.ts
@@ -7,61 +7,45 @@ import { type InMemoryEntity } from "../in_memory";
 export type SystemInSet = Required<SystemInSetSchema>;
 export type InSet = SystemInSet["inSet"][0];
 
-type InMemoryEntityInSetSchema = ReturnType<typeof schemaMixin>;
-
-function schemaMixin<E extends InMemoryEntity>(item: E) {
-    const schema = {
+export function inMemoryEntityInSetMixin<E extends InMemoryEntity>(item: E) {
+    // @ts-expect-error
+    const properties: SystemInSet & InSetPropertiesInMemoryEntity & E = {
         get inSet() {
-            return item.prop<InSet[]>("inSet", []);
+            return this.prop<InSet[]>("inSet", []);
         },
 
         set inSet(inSet: InSet[]) {
-            item.setProp("inSet", inSet);
+            this.setProp("inSet", inSet);
         },
-    } satisfies SystemInSetSchema;
 
-    Object.defineProperties(item, Object.getOwnPropertyDescriptors(schema));
-
-    return schema;
-}
-
-function propertiesMixin<E extends InMemoryEntity>(item: E & InMemoryEntityInSetSchema) {
-    const properties = {
         getInSetFilteredByCls(cls: string) {
-            return item.inSet.filter((ref) => ref.cls === cls);
+            return this.inSet.filter((ref) => ref.cls === cls);
         },
 
         // finds a parent entity set of the same cls (hence `cls` field is absent)
         // NOTE: assumes that only one entry of this kind is present => gets the first one
         get parentEntitySetReference() {
-            return item.inSet.find((item) => item._id && !item.cls);
+            return this.inSet.find((inSetItem) => inSetItem._id && !inSetItem.cls);
         },
     };
 
     Object.defineProperties(item, Object.getOwnPropertyDescriptors(properties));
-
-    return properties;
 }
 
-export function inMemoryEntityInSetMixin<E extends InMemoryEntity>(item: E) {
-    return {
-        ...schemaMixin(item),
-        ...propertiesMixin(item as E & InMemoryEntityInSetSchema),
-    };
-}
+export type InSetPropertiesInMemoryEntity = {
+    getInSetFilteredByCls: (cls: string) => InSet[];
+    parentEntitySetReference: InSet | undefined;
+};
 
-export type InMemoryEntityInSet = ReturnType<typeof inMemoryEntityInSetMixin>;
+export type InMemoryEntityInSet = SystemInSet & InSetPropertiesInMemoryEntity;
 export type InMemoryEntityInSetConstructor = Constructor<InMemoryEntityInSet>;
 
 type Base = Constructor<InMemoryEntity>;
 
 export default function InMemoryEntityInSetMixin<S extends Base = Base>(superclass: S) {
-    class InMemoryEntityInSetMixin extends superclass {
-        constructor(...args: any[]) {
-            super(...args);
-            inMemoryEntityInSetMixin(this);
-        }
-    }
+    class InMemoryEntityInSetMixin extends superclass {}
+
+    inMemoryEntityInSetMixin(InMemoryEntityInSetMixin.prototype);
 
     return InMemoryEntityInSetMixin as S & InMemoryEntityInSetConstructor;
 }
