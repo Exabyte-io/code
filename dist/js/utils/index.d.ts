87c2a04f52637b928c806dc6ab42b47901670b58
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-04-22 22:54:25 +0300

    chore: remove extendThis

--- dist/js/utils/index.d.ts
+++ dist/js/utils/index.d.ts
@@ -1,6 +1,6 @@
 import { compareEntitiesInOrderedSetForSorting } from "../entity/set/ordered/utils";
 import { convertToCompactCSVArrayOfObjects, safeMakeArray } from "./array";
-import { cloneClass, extendClass, extendClassStaticProps, extendThis } from "./class";
+import { cloneClass } from "./class";
 import { deepClone } from "./clone";
 import { refreshCodeMirror } from "./codemirror";
 import { createObjectPathFromFilePath, formatFileSize, getDirectories, getFilesInDirectory, getProgrammingLanguageFromFileExtension } from "./file";
@@ -16,4 +16,4 @@ import { mapTree } from "./tree";
 import { containsEncodedComponents } from "./url";
 import { getUUID } from "./uuid";
 import { JsYamlAllSchemas, JsYamlTypes } from "./yaml";
-export { compareEntitiesInOrderedSetForSorting, safeMakeArray, convertToCompactCSVArrayOfObjects, cloneClass, extendThis, extendClass, extendClassStaticProps, deepClone, refreshCodeMirror, getOneMatchFromObject, safeMakeObject, convertKeysToCamelCaseForObject, flattenObject, renameKeysForObject, sortKeysDeepForObject, stringifyObject, getProgrammingLanguageFromFileExtension, formatFileSize, calculateHashFromObject, calculateHashFromString, removeTimestampableKeysFromConfig, removeNewLinesAndExtraSpaces, randomAlphanumeric, removeEmptyLinesFromString, removeCommentsFromSourceCode, toFixedLocale, getUUID, getSearchQuerySelector, containsEncodedComponents, convertArabicToRoman, setUnitsHead, setNextLinks, addUnit, removeUnit, replaceUnit, mapTree, getSchemaWithDependencies, mergeTerminalNodes, JsYamlTypes, JsYamlAllSchemas, renderTextWithSubstitutes, filterEntityList, getFilesInDirectory, getDirectories, createObjectPathFromFilePath, buildNamedEntitySchema, findPreviousVersion, fetchFilesFromGitHubAPI, };
+export { compareEntitiesInOrderedSetForSorting, safeMakeArray, convertToCompactCSVArrayOfObjects, cloneClass, deepClone, refreshCodeMirror, getOneMatchFromObject, safeMakeObject, convertKeysToCamelCaseForObject, flattenObject, renameKeysForObject, sortKeysDeepForObject, stringifyObject, getProgrammingLanguageFromFileExtension, formatFileSize, calculateHashFromObject, calculateHashFromString, removeTimestampableKeysFromConfig, removeNewLinesAndExtraSpaces, randomAlphanumeric, removeEmptyLinesFromString, removeCommentsFromSourceCode, toFixedLocale, getUUID, getSearchQuerySelector, containsEncodedComponents, convertArabicToRoman, setUnitsHead, setNextLinks, addUnit, removeUnit, replaceUnit, mapTree, getSchemaWithDependencies, mergeTerminalNodes, JsYamlTypes, JsYamlAllSchemas, renderTextWithSubstitutes, filterEntityList, getFilesInDirectory, getDirectories, createObjectPathFromFilePath, buildNamedEntitySchema, findPreviousVersion, fetchFilesFromGitHubAPI, };
